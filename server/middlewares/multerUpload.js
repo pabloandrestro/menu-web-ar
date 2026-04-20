9477e5239d858a0957f29b5b1f7651b543bf9290
Sebastián Vergara Muñoz <sa.vergaramunoz@gmail.com>
2026-04-20 12:36:04 -0400

    feat: implement multer middleware for 3D model uploads

---
+++ server/middlewares/multerUpload.js
@@ -0,0 +1,34 @@
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
+const multer = require("multer");
+
+const storageModels = new CloudinaryStorage({
+  cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "models",
+      resource_type: "raw",
+      public_id: `${Date.now()}-${file.originalname}`,
+      format: file.originalname.split(".").pop(),
+    };
+  },
+});
+
+const uploadModels = multer({
+  storage: storageModels,
+  limits: { fileSize: 30 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const allowed = ["glb"];
+    const ext = file.originalname.split(".").pop();
+
+    if (allowed.includes(ext)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Solo se permiten archivos .glb"));
+    }
+  },
+}); // 30MB
+
+module.exports = {
+  uploadModels,
+};
