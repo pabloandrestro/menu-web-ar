d0980c3843a96740e7bb6e15586cba0b16f5b5ef
Sebastián Vergara Muñoz <sa.vergaramunoz@gmail.com>
2026-04-20 15:44:07 -0400

    feat: replace Cloudinary storage with memory storage in multer middleware and add uploadModelToCloudinary service

--- server/middlewares/multerUpload.js
+++ server/middlewares/multerUpload.js
@@ -1,22 +1,7 @@
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const { cloudinaryModelsFolder } = require("../config/guardEnv");
-const cloudinary = require("../config/cloudinary");
 const multer = require("multer");
 
-const storageModels = new CloudinaryStorage({
-  cloudinary,
-  params: async (req, file) => {
-    return {
-      folder: cloudinaryModelsFolder,
-      resource_type: "raw",
-      public_id: `${Date.now()}-${file.originalname}`,
-      format: file.originalname.split(".").pop(),
-    };
-  },
-});
-
 const uploadModels = multer({
-  storage: storageModels,
+  storage: multer.memoryStorage(),
   limits: { fileSize: 30 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const allowed = ["glb"];
